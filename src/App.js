ff79ab27a24eb3fabc25ee782ec7b61bb65c3f5c
sourabhagrawal23 <sourabhkhs23@gmail.com>
2021-04-12 01:21:04 +0530

    Finished: UI Layer Version 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 function App() {
 
-  const [tasks, setTasks] = useState(
-    [
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState
+    ([
       {
         id: 1,
         text: 'Doctors Appointment',
@@ -24,7 +27,16 @@ function App() {
         reminder: false
       }
     ]
-  )
+    )
+
+  // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //Delete task
 
   const deleteTask = (id) => {
@@ -34,13 +46,14 @@ function App() {
   ///Toggle Reminder
 
   const toggleReminder = ((id) => {
-    alert({ id });
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   })
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ?
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         : 'No task to Show'}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,23 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
-const Header = ({
-    title
-}) => {
-    const onClick = () => {
-        alert('text');
-    }
-    return (<
-        header className='header' >
-        <
-        h1 style={
-                {
-                    color: 'red'
-                }
-            } > {
-                title
-            } < /h1> <
-                Button color='green'
-                onClick={
-                    onClick
-                }
-                text='Hello' />
-            <
-        /header>
+const Header = ({ title, onAdd, showAdd }) => {
+
+    return (<header className='header' >
+        <h1 style={{ color: 'Gray' }}>
+            {title} </h1> <
+            Button color={showAdd ? 'red' : 'lightgreen'}
+            onClick={
+                onAdd
+            }
+            text={showAdd ? 'Close' : 'Add Task'} />
+    </header>
     )
 }
 Header.defaultProps = {
-                title: 'Task tracker'
+    title: 'Task tracker'
 }
 
 Header.propTypes = {
-                title: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
 }
 export default Header
